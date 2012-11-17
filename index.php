34c8ec43448f7ad64d24d1707e605978e60a0fd0
PavelPronskiy <pavel@pronskiy.ru>
2012-11-18 03:22:12 +0400

    Deleting deprecated sources

--- index.php
+++ index.php
@@ -8,11 +8,10 @@
 	//$channels = $tvtuner->getChannelsArray('SELECT * FROM stations', $formatOutput);
 	$queryString = explode('/', $_SERVER["QUERY_STRING"]);
 
-	if (isset($queryString[1])) {
+	if (isset($queryString[1]))
 		$queryData = $queryString[1];
-	} else {
+	else
 		$queryData = null;
-	}
 
 
 	$queryMethod = $queryString[0];
@@ -47,36 +46,5 @@
 		require_once('head.php');
 		require_once('foot.php');
 	}
-
-
-
-	
-/*
-	include_once('lib.php');
-	
-	//create_sq_db($station_db);
-	
-	$view_channel_rows = "SELECT * FROM stations ORDER BY number ASC";
-	//$view_channel_row_detail = "SELECT * FROM stations WHERE name='3'";
-	
-	$arr = array();
-	
-	
-	//$arr = delete_name_sq_db($station_db, 3);
-
-	$arr = query_sq_db($station_db,$view_channel_rows);
-	$json_out = query_sq_json_db($station_db,$view_channel_rows);
-	$cn = 0;
-
-
-	$r_name = $_REQUEST['name'];
-	$r_freq = $_REQUEST['freq'];
-
-	if (!empty($r_freq) and !empty($r_name)) {
-		change_freq($r_name,$r_freq);
-		exit;
-	}
-*/
-
 
 ?>
